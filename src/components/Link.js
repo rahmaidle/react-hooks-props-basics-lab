328934439c66148e6a6960ff6b968026caf64c15
rahmaidle <rahmaidle254@gmail.com>
2024-08-28 10:38:20 +0300

    complete lab

---
+++ src/components/Link.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import user from '../data/user'
+
+export default function Link() {
+  return (
+    <div>
+      <h3>Links</h3>
+      <a href={user.links.github}>{user.links.github}</a>
+      <a href={user.links.linkedin}>{user.links.linkedin}</a>
+    </div>
+  )
+}
